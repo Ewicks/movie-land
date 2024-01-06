680c854a55bd3ec6f14706186b99ba0898621487
ethan <ethanowicks9@gmail.com>
2024-01-06 15:30:16 +0000

    Map over movies and dynamically pass as a prop

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import SearchIcon from './search.svg';
 import MovieCard from './MovieCard';
 
@@ -12,10 +12,12 @@ const movie1 = {Title: 'Superman, Spiderman or Batman', Year: '2011', imdbID: 't
 
 const App = () => {
 
+  const [movies, setMovies] = useState([]);
+
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
-    console.log(data.Search)
+    setMovies(data.Search)
   }
 
   useEffect(() => {
@@ -33,9 +35,23 @@ const App = () => {
         <img src={SearchIcon} alt='search' onClick={() => {}} />
       </div>
 
-      <div className='container'>
-        <MovieCard movie1={movie1} />
-      </div>
+      {
+        movies?.length > 0
+        ? (
+          <div className='container'>
+            {movies.map((movie) => (
+              <MovieCard movie={movie}/>
+            ))}
+          </div>
+        ) : 
+        (
+          <div className='empty'>
+            <h2>No movies found </h2>
+          </div>
+        )
+      }
+
+     
 
     </div>
   );
