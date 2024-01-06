694fe2ffc978782bbf85e93e747a9a31b0210686
ethan <ethanowicks9@gmail.com>
2024-01-06 15:16:45 +0000

    Add moviecard component to new file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
 import { useEffect } from 'react';
+import SearchIcon from './search.svg';
+import MovieCard from './MovieCard';
 
 // 8d2475b6
 
 const API_URL = 'http://www.omdbapi.com?apikey=8d2475b6'
+
+const movie1 = {Title: 'Superman, Spiderman or Batman', Year: '2011', imdbID: 'tt2084949', Type: 'movie', Poster: 'N/A'}
+
 
 const App = () => {
 
@@ -21,7 +26,18 @@ const App = () => {
 
 
   return (
-    <h1>App</h1>
+    <div className='app'>
+      <h1>MovieLand</h1>
+      <div className='search'>
+        <input placeholder='Search for movies' value={'superman'} onChange={() => {}}/>
+        <img src={SearchIcon} alt='search' onClick={() => {}} />
+      </div>
+
+      <div className='container'>
+        <MovieCard movie1={movie1} />
+      </div>
+
+    </div>
   );
 }
 
